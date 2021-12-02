4171852abd144f7cde1cd424f568c1c136c23926
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-12-01 23:12:29 -0300

    Login Admin

--- AppLivraria_TsT/Controllers/DashbordController.cs
+++ AppLivraria_TsT/Controllers/DashbordController.cs
@@ -11,7 +11,27 @@ namespace AppLivraria_TsT.Controllers
         // GET: Dashbord
         public ActionResult Index()
         {
+            if ((Session["usuarioLogado"] == null) || (Session["senhaLogado"] == null))
+
+            {
+                return RedirectToAction("LoginAdmin", "Login");
+            }
+            else
+            {
+                if (Session["tipologado2"] == null)
+                {
+                    return RedirectToAction("semAcesso", "DashBord");
+                    
+                }
+                ViewBag.message = "Você não tem acesso a essa página";
+            }
             return View();
+        }
+        public ActionResult semAcesso()
+        {
+            Response.Write("<script>alert('Você não tem acesso a essa página')</script>");
+            ViewBag.message = "Você não tem acesso a essa página";
+            return RedirectToAction("LoginAdmin", "Login");
         }
     }
 }
